5f37c6a1321b1b4181d158cf6754fc3845173856
MBernackiProgrammer <101584672+MBernackiProgrammer@users.noreply.github.com>
2022-04-04 20:24:02 +0200

    Enter

---
+++ PYTHONbasic/3IsDivisibleBy2/IsDivisibleBy2.py
@@ -0,0 +1,9 @@
+print("Please write number")
+Number = 0
+Number = input("Your number : ")
+Number = int(Number)
+
+if (Number % 2) < 1:
+    print("Your number is divisible by 2")
+else:
+    print("Your number is not divisible by 2")
