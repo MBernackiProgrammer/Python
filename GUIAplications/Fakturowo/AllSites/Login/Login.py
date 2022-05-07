8c99c8a5182d6b2c0d16d749c0fc6550721983e5
MBernackiProgrammer <101584672+MBernackiProgrammer@users.noreply.github.com>
2022-05-08 00:38:09 +0200

    Enter

--- GUIAplications/Fakturowo/AllSites/Login/Login.py
+++ GUIAplications/Fakturowo/AllSites/Login/Login.py
@@ -1,44 +1,21 @@
+##########################################################
+#
+# Tasks for this source / zadania tego pliku
+#   1.
+#   2.
+#   3.
+#   4.
+#   5.
+#
+##########################################################
 import PySimpleGUI as sg
 
-
 class Login:
-    loged = bool(True)
+    loged = bool(False)
     Password = ""
     Login = ""
 
-    def IDErrorSubsystemLogin():
-        print("Error")
-        Layout = [
-            [sg.Text("Sorry, but we can't login you to your account")],
-            [sg.Button("Try again"), sg.Button("Select another option of logining")]
-        ]
-
-        window = sg.Window('Fakturowo', Layout, size = (750,400))
-        while True:                             
-            event, values = window.read()
-            if event in (None, 'Exit', 'Cancel'):
-                break
-
-            if event == 'Try again':
-                Login.LoginByID()
-                window.close()
-
-            if event == 'Select another option of logining':
-                Login.LoginSite()
-                window.close()
-
-    def EMailErrorSubsystemLogin():
-        print("Error")
-
-    def IDLoginSubsystem(ID, Passworld):
-        if(ID == "1234" and Passworld == "1234"):
-            Login.Password = Passworld
-            Login.Login = ID
-            loged = bool(True)
-            print("Launch program")
-        else:
-            Login.IDErrorSubsystemLogin()
-
+# Main page og logging/ stona glowna logowania
     def LoginSite(self):
         Layout = [
             [sg.Text("Login by: ")],
@@ -53,14 +30,15 @@ class Login:
                 break
 
             if event == 'Workers ID':
-                Login.LoginByID()
+                Login.LoginByID(self)
                 window.close()
 
             if event == 'Workers E-Mail':
-                Login.LoginByEMail()
+                Login.LoginByEMail(self)
                 window.close()
 
-    def LoginByID():
+    #Login by company ID
+    def LoginByID(self):
         print("Login by ID")
         Layout = [
             [sg.Text("Your ID :") ],
@@ -76,15 +54,15 @@ class Login:
             if event in (None, 'Exit', 'Cancel'):
                 break
             if event == 'Return':
-                Login.LoginSite()
+                Login.LoginSite(self)
                 window.close()
         
             if event == 'Login':
-                Login.IDLoginSubsystem(values[0], values[1])
+                Login.IDLoginSubsystem(self ,values[0], values[1])
                 window.close()
 
-
-    def LoginByEMail():
+    #Login by business e-mail
+    def LoginByEMail(self):
         print("Login by e - mail")
         Layout = [
             [sg.Text("Something")],
@@ -98,3 +76,45 @@ class Login:
             event, values = window.read()
             if event in (None, 'Exit', 'Cancel'):
                 break
+
+# Login to FOS / logowanie do FOS
+
+    def IDLoginSubsystem(self,ID, Passworld):
+        if(ID == "1234" and Passworld == "1234"):
+            Login.Password = Passworld
+            Login.Login = ID
+            self.loged = bool(True)
+            print("Launch program")
+        else:
+            Login.IDErrorSubsystemLogin(self)
+
+# Errors messages / wiadomosci o blendach
+    #Logging by ID errors / błędy logowania przez ID
+    def IDErrorSubsystemLogin(self):
+        print("Error")
+        Layout = [
+            #error content / treść błędu 
+            [sg.Text("Sorry, but we can't login you to your account")],
+            #options to select / opcje do wyboru 
+            [sg.Button("Try again"), sg.Button("Select another option of logining")]
+        ]
+
+        window = sg.Window('Fakturowo', Layout, size = (750,400))
+        while True:                             
+            event, values = window.read()
+            if event in (None, 'Exit', 'Cancel'):
+                break
+            
+            #If press "Try again" button / jeśli użytkownik naciśnie przycisk "Try again"
+            if event == 'Try again':
+                Login.LoginByID(self)
+                window.close()
+
+            #If press "Select another option of logining" button / jeśli użytkownik naciśnie przycisk "Select another option of logining"
+            if event == 'Select another option of logining':
+                Login.LoginSite(self)
+                window.close()
+
+    #Logging by E - Mail errors / błędy logowania przez e-mail
+    def EMailErrorSubsystemLogin(self):
+        print("Error")
