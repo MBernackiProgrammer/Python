7f567d17a89b91b6305b27742e75ea0228adcbc9
MBernackiProgrammer <101584672+MBernackiProgrammer@users.noreply.github.com>
2022-05-08 00:37:07 +0200

    Enter

--- GUIAplications/Fakturowo/RunPage.py
+++ GUIAplications/Fakturowo/RunPage.py
@@ -1,6 +1,24 @@
+##########################################################
+#
+# Tasks for this source / zadania tego pliku
+#   1. Login / Logowanie
+#       a) Create "Login" object / storzenie obiektu "Login"
+#       b) Spawn login site / utworzenie strony logowania
+#   2. 
+#   3. 
+#
+##########################################################
 import PySimpleGUI as sg
 from AllSites.Login.Login import*
 from AllSites.MainPage.AccountanWorker.AccountantMainPage import*
+
+class Worker:
+    IsAdmin = bool(False)
+    PositionIndex = int(-1)
+    WorkerID = int(-1)
+    WorkerEMain = "NONE"
+    WorkerPassword = "NONE"
+    WorkerName = "NONE"
 
 LoginSite = Login()
 LoginSite.LoginSite()
@@ -8,14 +26,15 @@ LoginSite.LoginSite()
 IndexOfPosition = int(1)
 MainPageSite = AccountantMainPage()
 
-if LoginSite.loged == True:
+while True:
+    if LoginSite.loged == True:
     # 1 = accountant worker
     # 2 = Warehouse worker
-    match IndexOfPosition:
-        case 1:
-            MainPageSite.SpawnMainPage(LoginSite.Login)
-
-    
-else:
-    print("Error")
+        match IndexOfPosition:
+            case 1:
+                MainPageSite.SpawnMainPage(LoginSite.Login)
+                break
+                
+    else: 
+        print("Error")
 
